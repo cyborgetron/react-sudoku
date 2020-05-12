ea55e0a47286fb48940625ab62d54f8c7199ca4d
Chris Borgert <chris.borgert@callawaycloudconsulting.com>
2020-05-12 08:18:01 -0600

    finish grid, style blocks and grid, add logic

--- react_sudoku/src/components/grid/styles/index.ts
+++ react_sudoku/src/components/grid/styles/index.ts
@@ -1,12 +1,48 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+//import { theme } from '../../../styles'
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0;
 `
 
 export const Row = styled.div`
-    display: flex;
-    flex-flow: row;
-`
+         ${({ theme }) => css`
+           display: flex;
+           flex-flow: row;
+
+           &:nth-child(1) {
+             div {
+               border-top: solid 4px ${theme.colors.black};
+             }
+           }
+           &:nth-child(3),
+           &:nth-child(6) {
+             div {
+               border-bottom: solid 3px ${theme.colors.black};
+               /* 3px to make up for the below row's top border */
+             }
+           }
+           &:nth-child(9) {
+             border-bottom: solid 4px ${theme.colors.black};
+             /* 3px to make up for the below row's top border */
+           }
+
+           div {
+             &:nth-child(1) {
+               border-left: solid 4px ${theme.colors.black};
+             }
+             &:nth-child(3),
+             &:nth-child(6),
+             &:nth-child(9) {
+               border-right: solid 4px ${theme.colors.black};
+               /* 3px to make up for the below row's top border */
+             }
+             &:nth-child(4),
+             &:nth-child(7) {
+               border-left: none;
+             }
+           }
+         `}
+       `
